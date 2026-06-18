6fd90d679ca57c123218b690bc474e7acc49e2e4
Maximiliano Diaz <maximiliano.diaz@quaterniongames.com.ar>
2026-06-18 15:48:36 -0300

    fix variables sin uso

--- Assets/_Scripts/DestructibleCrate.cs
+++ Assets/_Scripts/DestructibleCrate.cs
@@ -9,10 +9,7 @@ namespace _Scripts
         public GameObject vfxExplosionPrefab; 
         public int minScraps = 2;
         public int maxScraps = 5;
-
-        [Header("Efectos")]
-        public AudioClip breakSound;            
-
+        
         [Header("Física del 'Pop'")]
         public float popForce = 5f; 
 
@@ -31,7 +28,7 @@ namespace _Scripts
                 GameObject scrap = Instantiate(scrapPrefab, transform.position + Vector3.up * 0.5f, Quaternion.identity);
                 Instantiate(vfxExplosionPrefab, transform.position, Quaternion.identity);
                 Rigidbody scrapRb = scrap.GetComponent<Rigidbody>();
-                if (scrapRb != null)
+                if (scrapRb)
                 {
                     Vector3 randomDir = new Vector3(Random.Range(-1f, 1f), 1.5f, Random.Range(-1f, 1f)).normalized;
                     scrapRb.AddForce(randomDir * popForce, ForceMode.Impulse);
