f4f79604e92e4635aeae79961225bf1f51d78c57
Maxiado <maximiliano.diaz@quaterniongames.com.ar>
2026-06-17 21:22:47 -0300

    Particulas 02 para segundo parcial. Explosión y nube al recoger objetos.

--- Assets/_Scripts/DestructibleCrate.cs
+++ Assets/_Scripts/DestructibleCrate.cs
@@ -6,6 +6,7 @@ namespace _Scripts
     {
         [Header("Recompensas")]
         public GameObject scrapPrefab; 
+        public GameObject vfxExplosionPrefab; 
         public int minScraps = 2;
         public int maxScraps = 5;
 
@@ -28,7 +29,7 @@ namespace _Scripts
             {
               
                 GameObject scrap = Instantiate(scrapPrefab, transform.position + Vector3.up * 0.5f, Quaternion.identity);
-                
+                Instantiate(vfxExplosionPrefab, transform.position, Quaternion.identity);
                 Rigidbody scrapRb = scrap.GetComponent<Rigidbody>();
                 if (scrapRb != null)
                 {
